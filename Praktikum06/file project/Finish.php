ec3e034fec2c3620f7c0f422576e73d994271465
KharismaAdindaPutriVidiasari <71319030+KharismaAdindaPutriVidiasari@users.noreply.github.com>
2021-04-29 21:38:35 +0700

    Delete Praktikum06 directory

--- Praktikum06/file project/Finish.php
+++
@@ -1,8 +0,0 @@
-<?php
-echo "SELAMATT, ANGKA YANG ANDA TEBAK BENAR.. HOREEE!! " .$_COOKIE['bilanganacak'];
-setcookie("bilanganacak", "", time()-3*30*24*3600,"/");
-echo "<br>";
-echo "<br>";
-echo "<a href='Start.php'>ulangi lagi</a>";
-echo "<- link untuk mengulangi permainan kembali"
-?>
